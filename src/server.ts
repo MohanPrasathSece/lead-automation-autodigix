b88ef2e6bde151716b3d5e073b568c2db9bab72e
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2026-07-31 16:35:15 +0530

    \new

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Routes
 app.use('/api/leads', webhookRoutes);
